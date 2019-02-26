72991ec6e75e9e7366b44cf769fa77abc00dd3e3
alexsobolenko <alexsobolenko@gmail.com>
2019-02-27 02:57:30 +0300

    create view of entities

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -7,6 +7,9 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+use App\Entity\Author;
+use App\Entity\Book;
+
 class AppController extends AbstractController {
 
   public function index() {
@@ -14,25 +17,29 @@ class AppController extends AbstractController {
   }
 
   public function authorsList() {
+    $authors = $this->getDoctrine()->getRepository(Author::class)->findAll();
     $options = [
-      "title" => "authorsList",
-      "content" => "authorsList"
+      "title" => "Authors",
+      "authors" => $authors
     ];
     return $this->render("author-list.html.twig", $options);
   }
 
   public function authorEdit($id) {
+    $author = $this->getDoctrine()->getRepository(Author::class)->find($id);
     $options = [
-      "title" => "authorEdit",
-      "content" => "authorEdit"
+      "title" => "Edit ".$author->getName(),
+      "mode" => "edit",
+      "author" => $author
     ];
     return $this->render("author-form.html.twig", $options);
   }
 
   public function authorCreate() {
     $options = [
-      "title" => "authorCreate",
-      "content" => "authorCreate"
+      "title" => "Add new author",
+      "mode" => "create",
+      "author" => [ "id" => "", "name" => "", "books" => "" ]
     ];
     return $this->render("author-form.html.twig", $options);
   }
@@ -46,25 +53,44 @@ class AppController extends AbstractController {
   }
 
   public function booksList() {
+    $altBooks = [];
+    $books = $this->getDoctrine()->getRepository(Book::class)->findAll();
+    foreach ($books as $book) {
+      $author = $this->getDoctrine()->getRepository(Author::class)->find($book->getId());
+      $altBooks[] = [
+        "id" => $book->getId(),
+        "name" => $book->getName(),
+        "author" => $author->getName(),
+        "price" => $book->getPrice()
+      ];
+    }
     $options = [
       "title" => "booksList",
-      "content" => "booksList"
+      "books" => $altBooks
     ];
     return $this->render("book-list.html.twig", $options);
   }
 
   public function bookEdit($id) {
+    $book = $this->getDoctrine()->getRepository(Book::class)->find($id);
+    $author = $this->getDoctrine()->getRepository(Author::class)->find($book->getId());
+    $authors = $this->getDoctrine()->getRepository(Author::class)->findAll();
     $options = [
-      "title" => "bookEdit",
-      "content" => "bookEdit"
+      "title" => "Edit book \"".$book->getName()."\" (".$author->getName().")",
+      "mode" => "edit",
+      "book" => [ "id" => $book->getId(), "name" => $book->getName(), "author" => $author->getName(), "price" => $book->getPrice() ],
+      "authors" => $authors
     ];
     return $this->render("book-form.html.twig", $options);
   }
 
   public function bookCreate() {
+    $authors = $this->getDoctrine()->getRepository(Author::class)->findAll();
     $options = [
-      "title" => "bookCreate",
-      "content" => "bookCreate"
+      "title" => "Add new book",
+      "mode" => "create",
+      "book" => [ "id" => "", "name" => "", "author" => "", "price" => "" ],
+      "authors" => $authors
     ];
     return $this->render("book-form.html.twig", $options);
   }
